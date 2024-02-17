8426040ea787ebe09c26879ba4a1bd667678542b
BeeLulzsec <157348858+BeeDomp@users.noreply.github.com>
2024-02-17 14:02:14 +0700

    Update bot.py

--- AviaxMusic/core/bot.py
+++ AviaxMusic/core/bot.py
@@ -8,7 +8,7 @@ from ..logging import LOGGER
 
 class Aviax(Client):
     def __init__(self):
-        LOGGER(__name__).info(f"Starting Bot...")
+        LOGGER(__name__).info(f"Memulai Bot...")
         super().__init__(
             name="AviaxMusic",
             api_id=config.API_ID,
@@ -33,22 +33,22 @@ class Aviax(Client):
             )
         except (errors.ChannelInvalid, errors.PeerIdInvalid):
             LOGGER(__name__).error(
-                "Bot has failed to access the log group/channel. Make sure that you have added your bot to your log group/channel."
+                "Bot gagal mengakses grup/saluran log. Pastikan Anda telah menambahkan bot ke grup/saluran log Anda."
             )
             exit()
         except Exception as ex:
             LOGGER(__name__).error(
-                f"Bot has failed to access the log group/channel.\n  Reason : {type(ex).__name__}."
+                f"Bot gagal mengakses grup/saluran log.\n  Reason : {type(ex).__name__}."
             )
             exit()
 
         a = await self.get_chat_member(config.LOG_GROUP_ID, self.id)
         if a.status != ChatMemberStatus.ADMINISTRATOR:
             LOGGER(__name__).error(
-                "Please promote your bot as an admin in your log group/channel."
+                "Silakan promosikan bot Anda sebagai admin di grup/saluran log Anda."
             )
             exit()
-        LOGGER(__name__).info(f"Music Bot Started as {self.name}")
+        LOGGER(__name__).info(f"Bot Musik Dimulai sebagai {self.name}")
 
     async def stop(self):
         await super().stop()
